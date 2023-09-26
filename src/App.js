fc6c5f76b15c7e6dacb10aa40a34b58d7b6756bd
abubkar <abubakaraw8@gmail.com>
2023-09-26 15:17:16 +0500

    home compnent added router added application runing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className='w-full h-screen flex justify-center items-center bg-blue-500'>
-    <h1 className="text-3xl font-bold underline text-Grey-700">
-      REACT ROUTER PROJECT
-    </h1>
-    </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Layout from './Layout/Layout';
+
+import Home from './Home/Home';
+import About from './About/About';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout />,
+  children: [
+    {
+      path: "",
+      element: <Home/>
+    },
+    {
+      path: "about",
+      element: <About/>
+    }
+  ]
+}
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
