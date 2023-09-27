46e4c41456c3fdfa1df09062f63440ca0f8c61de
abubkar <abubakaraw8@gmail.com>
2023-09-27 16:05:52 +0500

    user file added

--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 import Layout from './Layout/Layout';
 
 import Home from './Home/Home';
 import About from './About/About';
+import Contact from './Contact/Contact';
+import User from './User/User';
 
-const router = createBrowserRouter([{
-  path: '/',
-  element: <Layout />,
-  children: [
-    {
-      path: "",
-      element: <Home/>
-    },
-    {
-      path: "about",
-      element: <About/>
-    }
-  ]
-}
-])
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout />}>
+      <Route path='' element = {<Home />} />
+      <Route path='about' element = {<About />} />
+      <Route path='contact' element = {<Contact />} />
+      <Route path='user/:userid' element = {<User />} />
+
+    </Route>
+  )
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
